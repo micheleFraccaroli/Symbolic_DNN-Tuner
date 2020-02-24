4ab2d0203e551b45d7ab11d30345ced7fb907521
michele fraccaroli <michelefraccaroli@gmail.com>
2020-02-24 12:44:09 +0100

    Bug fixed, update improvement_checker, update successively BO cycles and update symbolic choice

--- neural_network.py
+++ neural_network.py
@@ -15,16 +15,16 @@ from datasets.cifar_dataset import cifar_data
 
 class neural_network:
     def __init__(self, X_train, Y_train, X_test, Y_test, n_classes):
-        self.train_data = X_train[:1000]
-        self.train_labels = Y_train[:1000]
-        self.test_data = X_test[:600]
-        self.test_labels = Y_test[:600]
+        self.train_data = X_train
+        self.train_labels = Y_train
+        self.test_data = X_test
+        self.test_labels = Y_test
         self.train_data = self.train_data.astype('float32')
         self.test_data = self.test_data.astype('float32')
         self.train_data /= 255
         self.test_data /= 255
         self.n_classes = n_classes
-        self.epochs = 20
+        self.epochs = 200
 
     def build_network(self, params, new):
         """
